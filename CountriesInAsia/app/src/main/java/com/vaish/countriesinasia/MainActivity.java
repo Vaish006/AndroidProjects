ca60bbfad394f135647c5a10d61a140407f96f98
unknown <kongativaish@gmail.com>
2021-03-30 16:36:52 +0530

    Basic table view setup.

--- CountriesInAsia/app/src/main/java/com/vaish/countriesinasia/MainActivity.java
+++ CountriesInAsia/app/src/main/java/com/vaish/countriesinasia/MainActivity.java
@@ -1,14 +1,23 @@
 package com.vaish.countriesinasia;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity
+{
+    ListView listView;
+    String[] listItems = {"Item one","Item two", "Item three"};
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list);
+        adapter = new ArrayAdapter<>(this, R.layout.country_item, R.id.listText, listItems);
+        listView.setAdapter(adapter);
     }
 }
